6ddefc96d65609bc3da8bcf7bbf7f678d8e3a2cb
arqwore <60177171+kakashi-hatake3@users.noreply.github.com>
2024-03-12 08:14:26 +0300

    Update lab1.cpp

--- lab_1/lab1.cpp
+++ lab_1/lab1.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <unistd.h>
+#include <signal.h>
+#include <pthread.h>
 
 struct thread_data
 {
@@ -10,30 +13,59 @@ static void * proc1(void* arg){
     printf("start proc 1\n"); 
     thread_data* data = (thread_data*) arg;
     while(data->flag != 0){
-        //waiting...
+        putchar('1');
+        fflush(stdout);
+        sleep(1);
     }
+
+
+    printf("finish proc 1\n");
 
     pthread_exit(&data->exit_code);
 
-    printf("finish proc 1\n");
 }
 
 static void * proc2(void* arg){
     printf("start proc 2\n"); 
+
+
+    sigset_t set;
+    siginfo_t info;
+    union sigval value;
+
+    sigemptyset(&set);
+    sigaddset(&set, SIGUSR1);
+    pthread_sigmask(SIG_BLOCK, &set, NULL);
+
+
+
     thread_data* data = (thread_data*) arg;
     while(data->flag != 0){
-        //waiting...
+        putchar('2');
+        fflush(stdout);
+        sleep(1);
     }
+
+    sigwaitinfo(&set, &info);
+
+    printf("signal was taken in 2nd thread: id: %d, value: %d\n", info.si_signo, info.si_value.sival_int);
+
+
+    printf("finish proc 2\n");
+    
 
     pthread_exit(&data->exit_code);
 
-    printf("finish proc 2\n");
 }
 
 int main(){
     printf("program start\n");
     thread_data first_struct;
     thread_data second_struct;
+
+    union sigval value;
+
+    value.sival_int = 666;
 
     first_struct.flag = 1;
     second_struct.flag = 1;
@@ -46,17 +78,26 @@ int main(){
     pthread_create(&first_thread, NULL, proc1, &first_struct.flag);
     pthread_create(&second_thread, NULL, proc2, &second_struct.flag);
 
+
+
     printf("waiting for any key\n");
     getchar();
+
+
 
     first_struct.flag = 0;
     second_struct.flag = 0;
 
+    pthread_sigqueue(second_thread, SIGUSR1, value);
+
     int* result1;
     int* result2;
 
+
+
     pthread_join(first_thread, (void **)&result1);
     pthread_join(second_thread, (void **)&result2);
+
 
     printf("both treads have finished\n");
     printf("proc1 exit code: %d\n", *result1);
